a6d1de5c0dfafdd964f5927bc5027d2952949a71
Rehnkhan <rk1713335@gmail.com>
2024-11-29 20:34:52 +0530

    passed contentype parameter

--- Edge.py
+++ Edge.py
@@ -6,10 +6,11 @@ from gridfs import GridFS
 from pymongo import MongoClient
 import os
 import logging
+from bson import ObjectId
 from image_processing.process_img import process_img  
 from image_processing.process_vedio import process_video
 import datetime
-from bson import ObjectId
+
 
 def compute(task_id, db):
     try:
@@ -28,32 +29,29 @@ def compute(task_id, db):
         #adjusting the file name to avoid overwriting 
         filename, file_extension = os.path.splitext(filee.filename)
         
-        file_extension=file_extension.strip()
-        new_filename = task_id + file_extension
-        op_new_f_name="computed_"+task_id 
-        # Ensure the file extension is stripped of any leading/trailing whitespace
-  
-
-        # Construct the file path using the new filename
-        file_path = os.path.join(save_dir, new_filename)
-        # print(file_path)
-
-        # Save the file to the directory
+        new_filename=task_id+file_extension
         
+        file_path = os.path.join(save_dir, filename+file_extension)
+        
+        
+        # Save the file to the directory
         with open(file_path, "wb") as f:
             f.write(filee.read())
             logging.info("file saved successfully")
+        # if os.path.exists(file_path):
 
         if file_extension==".jpg" or file_extension==".png" or file_extension==".jpeg":
-            tasks.update_one({"_id": ObjectId(task_id)}, {"$set": {"started_at": datetime.datetime.now()}})
-            compute_img(file_path,op_new_f_name,filename,file_extension,task_id,fs)  
+            tasks.update_one({"_id":ObjectId(task_id)}, {"$set": {"started_at": datetime.datetime.now()}})
+            
+            compute_img(file_path,new_filename,filename,file_extension,task_id,fs)  
+            # print(file_path,new_filename,filename,file_extension,task_id,fs)
             tasks.update_one({"_id": ObjectId(task_id)}, {"$set": {"completed_at": datetime.datetime.now()}})
             
             logging.info(f"File {filee.filename} has been successfully saved ")
             
         elif file_extension==".mp4" or file_extension==".avi" or file_extension==".mov":
             tasks.update_one({"_id": ObjectId(task_id)}, {"$set": {"started_at": datetime.datetime.now()}})
-            compute_video(file_path,op_new_f_name,filename,file_extension,task_id,fs)
+            compute_video(file_path,new_filename,filename,file_extension,task_id,fs)
             tasks.update_one({"_id": ObjectId(task_id)}, {"$set": {"completed_at": datetime.datetime.now()}})
             logging.info(f"File {filee.filename} has been successfully saved ")
         else:
@@ -70,27 +68,30 @@ def compute(task_id, db):
 def compute_img(file_path,new_filename,filename,file_extension,task_id,fs):
     # Process the image (this function should save the computed image to a new file path)
     computed_file_path = process_img(file_path, new_filename, filename, file_extension, task_id)
-    print(computed_file_path)
+    
     # Find the old file by its task_id
     old_file = fs.find_one({"_id": ObjectId(task_id)})
     if old_file:
         # Retrieve the old file's metadata
         old_file_id = old_file._id
         old_metadata = old_file.metadata if old_file.metadata else {}
+        old_content_type = old_file.contentType if old_file.contentType else {}
 
         # Delete the old file
-        fs.delete(ObjectId(old_file_id))
+        fs.delete(old_file_id)
 
         # Upload the computed file with the same _id and metadata
         with open(computed_file_path, "rb") as f:
-            fs.put(f, _id=ObjectId(old_file_id), filename=filename+file_extension, metadata=old_metadata)
+            fs.put(f, _id=old_file_id, filename=filename, metadata=old_metadata,contentType=old_content_type)
 
     
     # print("File replaced successfully with the computed image.")
     logging.info("File replaced successfully with the computed image.")
     # Delete the computed file
-    os.remove(computed_file_path)
-    os.remove(file_path)
+    print(computed_file_path)
+    print(file_path)
+    # os.remove(computed_file_path)
+    # os.remove(file_path)
     return
 
 
@@ -103,13 +104,14 @@ def compute_video(file_path,new_filename,filename,file_extension,task_id,fs):
         # Retrieve the old file's metadata
         old_file_id = old_file._id
         old_metadata = old_file.metadata if old_file.metadata else {}
+        old_content_type = old_file.contentType if old_file.contentType else {}
 
         # Delete the old file
-        fs.delete(ObjectId(old_file_id))
+        fs.delete(old_file_id)
 
         # Upload the computed file with the same _id and metadata
         with open(computed_file_path, "rb") as f:
-            fs.put(f, _id=ObjectId(old_file_id), filename=filename+file_extension, metadata=old_metadata)
+            fs.put(f, _id=old_file_id, filename=filename, metadata=old_metadata, contentType=old_content_type)
 
     
     # print("File replaced successfully with the computed image.")
@@ -134,10 +136,10 @@ def run_worker(edge_id):
             if response.taskId:
                 # print(f"Worker {worker_id} received task: {response.taskId}")
                 client = MongoClient(os.getenv('MONGO_URL'))
-                # compute(response.taskId.split(","))
+                
                 for task_id in response.taskId.split(","):
                    
-                    compute(task_id,client['taskmaster'])
+                    compute(task_id, client['taskmaster'])
                    
                 # Process the task here
             else:
